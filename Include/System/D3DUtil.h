cde283efd28732ec402a0fec78f7f710fbc4ff56
Minpro <dpalstn91@gmail.com>
2020-08-01 17:08:03 +0900

    ThirdCommit

--- Include/System/D3DUtil.h
+++ Include/System/D3DUtil.h
@@ -28,6 +28,8 @@
 #pragma comment(lib, "dxgi.lib")
 #pragma comment(lib, "d3d11.lib")
 #pragma comment(lib, "d3dx11.lib")
+#pragma comment(lib, "d3dx11d.lib")
+#pragma comment(lib, "dxerr.lib")
 #pragma comment(lib, "d3dx10.lib")
 #pragma comment(lib, "d3dcompiler.lib")
 #pragma comment(lib, "dinput8.lib")
